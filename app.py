f6fb7298b741ef7bc023755cba481852298e0aa3
Sakher Shabil <sakhershabil01@gmail.com>
2026-03-20 03:54:41 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -751,5 +751,8 @@ def exercise(level, lesson_index):
 init_db()
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
